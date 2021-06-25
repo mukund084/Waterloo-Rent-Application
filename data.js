6336160d2389fa6a0fc272db763b2286347363ec
Jaylon <jlaidlaw@uwaterloo.ca>
2021-06-25 16:13:33 -0400

    changes

---
+++ data.js
@@ -0,0 +1,42 @@
+const data = {
+    residences: [
+        {
+            number: 1,
+            address: '100 King Street'},
+        {
+            number: 2,
+            address: '223 University Ave'},
+        {
+            number: 3,
+            address: '327 Regina Street'},
+        {
+            number: 4,
+            address: '97 Columbia Ave'},
+        {
+            number: 4,
+            address: '107 Lester Ave'},
+    ],
+    prices: [
+        {
+            number: 1,
+            rent: 500, },
+
+        {
+            number: 2,
+            rent: 625, },
+        
+        {
+            number: 3,
+            rent: 830, },
+
+        {
+            number: 4,
+            rent: 450, },
+
+        {
+            number: 5,
+            rent: 1240,
+        }
+    ]
+};
+export default data; 

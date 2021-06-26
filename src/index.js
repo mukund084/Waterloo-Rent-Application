c461782c4f519833f0a9e9ccb77635f23ed7ee69
Jaylon <jlaidlaw@uwaterloo.ca>
2021-06-25 20:42:33 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use (express.json());
+
 app.get('/api/v1/residences', (req,res)=> res.json(data.residences));
 
 app.get('/api/v1/residences/:id',(req,res)=>{
@@ -12,7 +13,20 @@ app.get('/api/v1/residences/:id',(req,res)=>{
     let residences = data.residences;
     let response = residences.find(residences => residences.number === number)
     if(!response) {
-        res.status(404).json({"message": `Residence with Number: ${id} doesn't exist`});
+        res.status(404).json({"message": `Residence with Number: ${number} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+app.get('/api/v1/prices', (req,res)=> res.json(data.prices));
+
+app.get('/api/v1/prices/:id',(req,res)=>{
+    let id = parseInt(req.params.id);
+    let prices = data.prices;
+    let response = prices.find(prices => prices.number === number)
+    if(!response) {
+        res.status(404).json({"message": `Price with Number: ${number} doesn't exist`});
     }
     res.json(response).status(200);
 

e53eddb0775db71da69a2c2f4aa7baf18c19bfbc
Jaylon <jlaidlaw@uwaterloo.ca>
2021-06-28 14:13:27 -0400

    changes and swagger doc

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import prices from "./routes/prices";
 import rooms from "./routes/rooms";
 import bathrooms from "./routes/bathrooms";
 import smoking from "./routes/smoking";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use (express.json());
+app.use ("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1", residences);
 app.use("/api/v1", prices);

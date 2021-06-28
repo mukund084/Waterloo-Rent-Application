831d70ebe17d84fa34b0e894212aa855c06aa6e8
Jaylon <jlaidlaw@uwaterloo.ca>
2021-06-28 12:44:20 -0400

    changes

--- data.js
+++ data.js
@@ -37,6 +37,72 @@ const data = {
             number: 5,
             rent: 1300,
         }
+    ],
+    rooms: [
+        {
+            number: 1,
+            rooms: 4, },
+
+        {
+            number: 2,
+            rooms: 3, },
+        
+        {
+            number: 3,
+            rooms: 4, },
+
+        {
+            number: 4,
+            rooms: 1, },
+
+        {
+            number: 5,
+            rooms: 5,
+        }
+    ],
+    smoking: [
+        {
+            number: 1,
+            indication: "no", },
+
+        {
+            number: 2,
+            indication: "no", },
+        
+        {
+            number: 3,
+            indication: "yes", },
+
+        {
+            number: 4,
+            indication: "yes", },
+
+        {
+            number: 5,
+            indication: "no",
+        }
+    ],
+    bathrooms: [
+        {
+            number: 1,
+            baths: 2, },
+
+        {
+            number: 2,
+            baths: 3, },
+        
+        {
+            number: 3,
+            baths: 4, },
+
+        {
+            number: 4,
+            baths: 1, },
+
+        {
+            number: 5,
+            baths: 5,
+        }
     ]
 };
 export default data; 

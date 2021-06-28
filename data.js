0ab3c80e82206603c2a1583092b36b4a5598ecd1
Jaylon <jlaidlaw@uwaterloo.ca>
2021-06-28 18:40:30 -0400

    changes

--- data.js
+++ data.js
@@ -1,107 +1,108 @@
-module.exports = {
+const data = {
     residences: [
         {
-            number: 1,
+            id: 1,
             address: '100 King Street'},
         {
-            number: 2,
+            id: 2,
             address: '223 University Ave'},
         {
-            number: 3,
+            id: 3,
             address: '327 Regina Street'},
         {
-            number: 4,
+            id: 4,
             address: '97 Columbia Ave'},
         {
-            number: 5,
+            id: 5,
             address: '107 Lester Ave'},
     ],
     prices: [
         {
-            number: 1,
+            id: 1,
             rent: 500, },
 
         {
-            number: 2,
+            id: 2,
             rent: 625, },
         
         {
-            number: 3,
+            id: 3,
             rent: 830, },
 
         {
-            number: 4,
+            id: 4,
             rent: 450, },
 
         {
-            number: 5,
+            id: 5,
             rent: 1300,
         }
     ],
     rooms: [
         {
-            number: 1,
+            id: 1,
             rooms: 4, },
 
         {
-            number: 2,
+            id: 2,
             rooms: 3, },
         
         {
-            number: 3,
+            id: 3,
             rooms: 4, },
 
         {
-            number: 4,
+            id: 4,
             rooms: 1, },
 
         {
-            number: 5,
+            id: 5,
             rooms: 5,
         }
     ],
     smoking: [
         {
-            number: 1,
+            id: 1,
             indication: "no", },
 
         {
-            number: 2,
+            id: 2,
             indication: "no", },
         
         {
-            number: 3,
+            id: 3,
             indication: "yes", },
 
         {
-            number: 4,
+            id: 4,
             indication: "yes", },
 
         {
-            number: 5,
+            id: 5,
             indication: "no",
         }
     ],
     bathrooms: [
         {
-            number: 1,
+            id: 1,
             baths: 2, },
 
         {
-            number: 2,
+            id: 2,
             baths: 3, },
         
         {
-            number: 3,
+            id: 3,
             baths: 4, },
 
         {
-            number: 4,
+            id: 4,
             baths: 1, },
 
         {
-            number: 5,
+            id: 5,
             baths: 5,
         }
     ]
 };
+export default data; 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import bathrooms from "./routes/bathrooms";
 import smoking from "./routes/smoking";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
+import data from '../data';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,10 +15,131 @@ app.use (express.json());
 app.use ("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1", residences);
+app.use("/api/v2", residences);
 app.use("/api/v1", prices);
+app.use("/api/v2", prices);
 app.use("/api/v1", rooms);
+app.use("/api/v2", rooms);
 app.use("/api/v1", bathrooms);
+app.use("/api/v2", bathrooms);
 app.use("/api/v1", smoking);
+app.use("/api/v2", smoking);
+
+app.get('/', (req, res) => {
+    res.send('test');
+});
+
+app.get('/api/v1/residences',(req,res)=> {
+    res.json(data.residences).status(200)
+});
+
+app.get('/api/v1/residences/:id',(req,res)=>{
+    let id = parseInt(req.params.id);
+    let residences = data.residences;
+    let response = residences.find(residences => residences.id === id)
+    if(!response) {
+        res.status(404).json({"message": `Residence with ID: ${id} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+app.get('/api/v2/residences/:address',(req,res)=>{
+    let address = (req.params.address);
+    let residences = data.residences;
+    let response = residences.find(residences => residences.address === address)
+    if(!response) {
+        res.status(404).json({"message": `Residence with address: ${address} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+app.get('/api/v1/prices', (req,res)=> res.json(data.prices));
+
+app.get('/api/v1/prices/:id',(req,res)=>{
+    let id = parseInt(req.params.id);
+    let prices = data.prices;
+    let response = prices.find(prices => prices.id === id)
+    if(!response) {
+        res.status(404).json({"message": `Residence with ID: ${id} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+app.get('/api/v2/prices/:price',(req,res)=>{
+    let rent = parseFloat(req.params.rent);
+    let prices = data.prices;
+    let response = prices.find(prices => prices.rent === rent)
+    if(!response) {
+        res.status(404).json({"message": `Residence with price: ${rent} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+
+app.get('/api/v1/rooms', (req,res)=> res.json(data.rooms));
+
+app.get('/api/v1/rooms/:id',(req,res)=>{
+    let id = parseInt(req.params.id);
+    let rooms = data.rooms;
+    let response = rooms.find(rooms => rooms.id === id)
+    if(!response) {
+        res.status(404).json({"message": `Residence with ID: ${id} doesn't exist`});
+    }
+    res.json(response).status(200);
+ 
+});
+
+app.get('/api/v1/bathrooms', (req,res)=> res.json(data.bathrooms));
+
+app.get('/api/v1/bathrooms/:id',(req,res)=>{
+    let id = parseInt(req.params.id);
+    let bathrooms = data.bathrooms;
+    let response = bathrooms.find(bathrooms => bathrooms.id === id)
+    if(!response) {
+        res.status(404).json({"message": `Residence with ID: ${id} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+app.get('/api/v2/bathrooms/:baths',(req,res)=>{
+    let baths = parseInt(req.params.baths);
+    let bathrooms = data.bathrooms;
+    let response = bathrooms.find(bathrooms => bathrooms.baths === baths)
+    if(!response) {
+        res.status(404).json({"message": `Residence with number of bathrooms: ${baths} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+app.get('/api/v1/smoking', (req,res)=> res.json(data.smoking));
+
+app.get('/api/v1/smoking/:id',(req,res)=>{
+    let id = parseInt(req.params.id);
+    let smoking = data.smoking;
+    let response = smoking.find(smoking => smoking.id === id)
+    if(!response) {
+        res.status(404).json({"message": `Residence with ID: ${id} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
+
+app.get('/api/v2/smoking/:indication',(req,res)=>{
+    let indication = (req.params.indication);
+    let smoking = data.smoking;
+    let response = smoking.find(smoking => smoking.indication === indication)
+    if(!response) {
+        res.status(404).json({"message": `Residence with Indication: ${indication} doesn't exist`});
+    }
+    res.json(response).status(200);
+
+});
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on port: ${PORT}`);

389190f50e1c8cb5ff7180bac54e39fea1eb7e50
Jaylon <jlaidlaw@uwaterloo.ca>
2021-06-28 21:25:32 -0400

    changes

--- uw-rent-react-app/src/App.js
+++ uw-rent-react-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import ResidencesList from './components/ResidencesList';
 
 function App(props) {
   return (
     <div>
+      <>
       <h1> {props.name} </h1>
         <img src="/images/homepage-image.jpg.jpg" alt="" />
+    <ResidencesList />
+    </>
     </div>
   );
 }
---
+++ uw-rent-react-app/src/components/ResidencesList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class ResidencesList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { residences: [] };
+    }
+    componentDidMount() {
+        fetch(
+            '/http://localhost:3001/api/v1/residences'
+        )
+        .then((response) => response.json())
+        .then((result) => this.setState({ residences: result }));
+    }
+
+    renderResidences() {
+        return this.state.residences.map((residence) => <div>{residence.address}</div>)
+    }
+
+    render () {
+        return (
+        <>
+        <h2> Residences List</h2>
+        {this.renderResidences()}
+        </>
+        );
+    }
+
+}
